5e08e5cf67dee4c5ae4a79794f93cf7f9171f167
SayanDasDev <mesayan19@gmail.com>
2024-08-15 23:12:38 +0530

    event count dynamically added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Events from "@/components/events";
 import HeroCardLeft from "@/components/hero-card-left";
 import HeroCardRight from "@/components/hero-card-right";
@@ -7,22 +7,22 @@ import HeroTitle from "@/components/hero-title";
 import { Logos } from "@/components/logos";
 import NavMenu from "@/components/nav-menu";
 import Navbar from "@/components/navbar";
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { useGSAP } from '@gsap/react';
-
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  
-
   return (
     <main className="no-scrollbar overflow-x-hidden">
       <NavMenu />
-      <section id="hero" className="hero w-screen h-screen grid grid-rows-[80px_25dvh_1fr] md:grid-rows-[80px_30dvh_1fr] xl:grid-rows-[80px_1fr_280px]">
+      <section
+        id="hero"
+        className="hero w-screen h-screen grid grid-rows-[80px_25dvh_1fr] md:grid-rows-[80px_30dvh_1fr] xl:grid-rows-[80px_1fr_280px]"
+      >
         <Navbar />
         <div className="text-gray-50 h-4 xl:hidden ml-auto mr-6 my-6" />
         <div className="w-full hero-title-grid xl:flex items-center justify-between mt-8 xl:mt-0 xl:px-6 xl:pb-8">
@@ -36,22 +36,22 @@ export default function Home() {
           <HeroCardRight />
         </div>
       </section>
-      <section id="about"  className="about w-screen h-screen flex items-center justify-center text-9xl bg-gray-700 text-white">
+      <section
+        id="about"
+        className="about w-screen h-screen flex items-center justify-center text-9xl bg-gray-700 text-white"
+      >
         about
       </section>
-      <section id="events" className="flex">
-        <Events />
-      </section>
-      {/* Dummy sections added for each 100% offset set in Events */}
-      <section className="about w-screen h-screen" />
-      <section className="about w-screen h-screen" />
-      <section className="about w-screen h-screen" />
 
-      
+      <Events />
+
       <section className="about w-screen h-screen bg-pink-200 text-9xl">
         Schedule
       </section>
-      <section id="gallery" className="about w-screen h-screen bg-green-200 text-9xl">
+      <section
+        id="gallery"
+        className="about w-screen h-screen bg-green-200 text-9xl"
+      >
         gallery
       </section>
     </main>
--- components/events.tsx
+++ components/events.tsx
@@ -1,49 +1,85 @@
-"use client"
+"use client";
 
-import { useLayoutEffect } from "react";
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { useGSAP } from '@gsap/react';
-
+import { useLayoutEffect, useState } from "react";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { useGSAP } from "@gsap/react";
 
 const Events = () => {
-
   const container = useRef<HTMLDivElement | null>(null);
 
-  useGSAP(() => {
-    gsap.to(".event", {
-            xPercent: -800,
-            ease: "none",
-            scrollTrigger: {
-              trigger: container.current,
-              pin: true,
-              scrub: 1,
-              snap: {
-                snapTo: 1 / 8,
-                duration: 0.05
-              },
-              // base vertical scrolling on how wide the container is so it feels more natural.
-              end: "300%"
-            }
-          });
-  }, {scope: container})
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useGSAP(
+    () => {
+      if (!isMounted) return;
+
+      let events = gsap.utils.toArray(".event");
+
+      gsap.to(".event", {
+        xPercent: -100 * (events.length - 1),
+        ease: "none",
+        scrollTrigger: {
+          trigger: container.current,
+          pin: true,
+          scrub: 1,
+          snap: {
+            snapTo: 1 / (events.length - 1),
+            duration: 0.05,
+          },
+          // base vertical scrolling on how wide the container is so it feels more natural.
+          end: "300%",
+        },
+      });
+    },
+    { scope: container, dependencies: [isMounted] }
+  );
 
   return (
-    <div ref={container} className="flex flex-nowrap w-[900%]">
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-red-200">Event 1</div>
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-cyan-200">Event 2</div>
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-amber-200">Event 3</div>
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-red-200">Event 4</div>
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-cyan-200">Event 5</div>
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-amber-200">Event 6</div>
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-red-200">Event 7</div>
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-cyan-200">Event 8</div>
-      <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-amber-200">Event 9</div>
-      {/* Add more sections as needed */}
-    </div>
+    <>
+      <section id="events" className="flex">
+        <div ref={container} className="flex flex-nowrap w-[900%]">
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-red-200">
+            Event 1
+          </div>
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-cyan-200">
+            Event 2
+          </div>
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-amber-200">
+            Event 3
+          </div>
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-red-200">
+            Event 4
+          </div>
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-cyan-200">
+            Event 5
+          </div>
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-amber-200">
+            Event 6
+          </div>
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-red-200">
+            Event 7
+          </div>
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-cyan-200">
+            Event 8
+          </div>
+          <div className="event h-screen w-screen flex items-center justify-center text-8xl bg-amber-200">
+            Event 9
+          </div>
+        </div>
+      </section>
+      {/* Dummy sections added for each 100% offset in the end*/}
+      <section className="about w-screen h-screen" />
+      <section className="about w-screen h-screen" />
+      <section className="about w-screen h-screen" />
+    </>
   );
 };
 
-export default Events
+export default Events;

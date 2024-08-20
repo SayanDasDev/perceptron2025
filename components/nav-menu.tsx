a5c9d01e1e5d090c9b37652997a4039c7ba8189a
SayanDasDev <mesayan19@gmail.com>
2024-08-20 20:38:58 +0530

    nav menu z index fixed

--- components/nav-menu.tsx
+++ components/nav-menu.tsx
@@ -25,6 +25,7 @@ const NavMenu = () => {
     .to('.menu-overlay', {
       duration: 0.25,
       height: '600px',
+      maxWidth: '400px',
       width: 'calc(100vw - 24px)',
     })
     .set('.nav-menu-links', {y: 20})
@@ -50,9 +51,9 @@ const NavMenu = () => {
 
   return (
     <div className="" ref={container}>
-      <Button onClick={toggleMenu} variant={"ghostNoHover"} className={`${font_sub.className} xl:hidden fixed top-5 right-3 z-10 text-background/50 text-xl`}>{isMenuOpen ? `Close` : `Menu`}</Button>
-      <div className="menu-overlay xl:hidden menu-bg-glassmorphic border border-background/20 fixed top-4 right-3 h-12 w-[100px] rounded-2xl" />
-      <div className={`${font_sub.className} xl:hidden fixed top-0 left-0 translate-x-[calc(50vw-50%)] translate-y-[15%] flex flex-col gap-4 items-center justify-center p-6 text-2xl text-gray-300`}>
+      <Button onClick={toggleMenu} variant={"ghostNoHover"} className={`${font_sub.className} xl:hidden fixed top-5 right-3 z-[100] text-background/50 text-xl`}>{isMenuOpen ? `Close` : `Menu`}</Button>
+      <div className="menu-overlay xl:hidden menu-bg-glassmorphic border border-background/20 fixed top-4 right-3 h-12 w-[100px] rounded-2xl z-[90]" />
+      <div className={`${font_sub.className} xl:hidden fixed top-0 right-0 translate-x-[calc(-50vw+50%)] min-[425px]:translate-x-[-110px] translate-y-[15%] flex flex-col gap-4 items-center justify-center p-6 text-2xl text-gray-300 z-[95]`}>
         {docsConfig.navbarItems.map((item) => (
           <Link className={`nav-menu-links opacity-0`} href={item.href} key={item.title}>{item.title}</Link>
         ))}

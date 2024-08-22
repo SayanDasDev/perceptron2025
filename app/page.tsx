a762bb61b1e890ccfcbd180921c21c4fe15f027a
SayanDasDev <mesayan19@gmail.com>
2024-08-22 21:58:59 +0530

    bg-video added

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,47 @@
 "use client";
 import About from "@/components/about";
+import BgVideo from "@/components/bg-video";
 import Events from "@/components/events";
 import EventsMobile from "@/components/events-mobile";
 import HeroCardLeft from "@/components/hero-card-left";
 import HeroCardRight from "@/components/hero-card-right";
-import HeroCTA from "@/components/hero-cta";
 import HeroTitle from "@/components/hero-title";
 import { Logos } from "@/components/logos";
 import NavMenu from "@/components/nav-menu";
 import Navbar from "@/components/navbar";
 import Schedule from "@/components/schedule";
+import { Button } from "@/components/ui/button";
+import useFonts from "@/hooks/useFonts";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Pause, Play } from "lucide-react";
 import Image from "next/image";
+import { useRef, useState } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const { sub } = useFonts();
+
+  const [videoPlaying, setVideoPlaying] = useState(false);
+
+  const playIconRef = useRef(null);
+  const bgRef = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap.to(playIconRef.current, {
+        rotate: videoPlaying ? 360 : 0,
+        scale: videoPlaying ? 0.8 : 1,
+        duration: 0.3,
+        ease: "back.out(1.7)"
+      });
+
+    },
+    { scope: "#hero", dependencies: [videoPlaying] }
+  );
+
   return (
     <main className="overflow-x-hidden">
       <NavMenu />
@@ -24,8 +49,13 @@ export default function Home() {
         id="hero"
         className="hero w-screen h-screen grid grid-rows-[80px_25dvh_1fr] md:grid-rows-[80px_30dvh_1fr] xl:grid-rows-[80px_1fr_280px]"
       >
-        <Image src={"/images/hero.png"} alt="" fill objectFit="cover" />
-
+        {/* <div ref={bgRef}> */}
+          {videoPlaying ? (
+            <BgVideo />
+          ) : (
+            <Image src={"/images/hero.png"} alt="" fill objectFit="cover" />
+          )}
+        {/* </div> */}
         <div className="overlay absolute inset-0  bg-black/50" />
         <Navbar />
         <div className="text-gray-50 h-4 xl:hidden ml-auto mr-6 my-6" />
@@ -36,7 +66,29 @@ export default function Home() {
         </div>
         <div className="grid hero-cards-grid">
           <HeroCardLeft />
-          <HeroCTA />
+          <div className="hero-cta flex flex-col-reverse col-span-1 md:col-span-2 xl:col-span-1 xl:flex-col gap-6 items-center justify-center">
+            <Button
+              className={`${sub.className} xl:translate-y-[-150%]`}
+              variant={"heroCTA"}
+              size={"responsiveXL"}
+            >
+              Register
+            </Button>
+            <Button
+              onClick={() => setVideoPlaying(!videoPlaying)}
+              className={`${sub.className} hero-play rounded-full w-20 h-20 xl:w-28 xl:h-28 px-0`}
+              variant={"heroCTA"}
+              size={"responsiveXL"}
+            >
+              <div ref={playIconRef}>
+                {videoPlaying ? (
+                  <Pause className="w-8 h-8 xl:w-12 xl:h-12" />
+                ) : (
+                  <Play className="w-8 h-8 xl:w-12 xl:h-12" />
+                )}
+              </div>
+            </Button>
+          </div>
           <HeroCardRight />
         </div>
       </section>
@@ -58,10 +110,7 @@ export default function Home() {
       </div>
 
       <Schedule />
-      <section
-        id="gallery"
-        className="w-screen h-screen bg-green-200 text-9xl"
-      >
+      <section id="gallery" className="w-screen h-screen bg-green-200 text-9xl">
         gallery
       </section>
     </main>

da7c33873e0c0f4a2c94dd7f299abc69b916f624
SayanDasDev <mesayan19@gmail.com>
2024-11-16 00:34:11 +0530

    countdown works

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -1,27 +1,60 @@
 import useFonts from '@/hooks/useFonts'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Countdown = () => {
 
   const { sub } = useFonts()
 
+  interface TimeLeft {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }
+
+  const calculateTimeLeft = (): TimeLeft => {
+    const difference = +new Date('2025-01-10') - +new Date();
+    let timeLeft: TimeLeft = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
   return (
     <div className={`${sub.className} text-gray-50 flex justify-around px-4 items-center`}>
       <div className="flex flex-col text-center text-xs">
-        <p className="text-2xl">199</p>
-        <p className="">Days</p>
-      </div>:
+        <p className="text-2xl">{timeLeft.days || '0'}</p>
+        <p>Days</p>
+      </div>
       <div className="flex flex-col text-center text-xs">
-        <p className="text-2xl">23</p>
-        <p className="">Hours</p>
-      </div>:
+        <p className="text-2xl">{timeLeft.hours || '0'}</p>
+        <p>Hours</p>
+      </div>
       <div className="flex flex-col text-center text-xs">
-        <p className="text-2xl">59</p>
-        <p className="">Minutes</p>
-      </div>:
+        <p className="text-2xl">{timeLeft.minutes || '0'}</p>
+        <p>Minutes</p>
+      </div>
       <div className="flex flex-col text-center text-xs">
-        <p className="text-2xl">59</p>
-        <p className="">Seconds</p>
+        <p className="text-2xl">{timeLeft.seconds || '0'}</p>
+        <p>Seconds</p>
       </div>
     </div>
   )
